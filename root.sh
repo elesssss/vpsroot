6e71ee8ed4ede41154661dd34c6479983c8faca4
elesssss <162124503+elesssss@users.noreply.github.com>
2024-04-21 14:39:09 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -24,96 +24,85 @@ check_release(){
     if [[ -e /etc/os-release ]]; then
         . /etc/os-release
         release=$ID
+        os_version=$VERSION_ID
     elif [[ -e /usr/lib/os-release ]]; then
         . /usr/lib/os-release
         release=$ID
-    else
-        echo ""
+        os_version=$VERSION_ID
     fi
 
-    os_version=$(grep -i version_id /etc/os-release | cut -d \" -f2 | cut -d . -f1)
 
     if [[ "${release}" == "arch" ]]; then
-        echo ""
-    elif [[ "${release}" == "parch" ]]; then
-        echo ""
-    elif [[ "${release}" == "manjaro" ]]; then
-        echo ""
-    elif [[ "${release}" == "armbian" ]]; then
-        echo ""
+        echo
+    elif [[ "${release}" == "kali" ]]; then
+        echo
     elif [[ "${release}" == "centos" ]]; then
-        if [[ ${os_version} -lt 7 ]]; then
-            echo -e "${Error} 请使用 CentOS 7 或更高版本！" && exit 1
-        fi
+        echo
     elif [[ "${release}" == "ubuntu" ]]; then
-        if [[ ${os_version} -lt 20 ]]; then
-            echo -e "${Error} 请使用 Ubuntu 20.04 或更高版本！" && exit 1
-        fi
+        echo
     elif [[ "${release}" == "fedora" ]]; then
-        if [[ ${os_version} -lt 36 ]]; then
-            echo -e "${Error} 请使用 Fedora 36 或更高版本！" && exit 1
-        fi
+        echo
     elif [[ "${release}" == "debian" ]]; then
-        if [[ ${os_version} -lt 10 ]]; then
-            echo -e "${Error} 请使用 Debian 10 或更高版本！" && exit 1
-        fi
+        echo
     elif [[ "${release}" == "almalinux" ]]; then
-        if [[ ${os_version} -lt 9 ]]; then
-            echo -e "${Error} 请使用 AlmaLinux 9 或更高版本！" && exit 1
-        fi
+        echo
     elif [[ "${release}" == "rocky" ]]; then
-        if [[ ${os_version} -lt 9 ]]; then
-            echo -e "${Error} 请使用 Rocky Linux 9 或更高版本！" && exit 1
-        fi
+        echo
     elif [[ "${release}" == "oracle" ]]; then
-        if [[ ${os_version} -lt 8 ]]; then
-            echo -e "${Error} 请使用 Oracle Linux 8 或更高版本！" && exit 1
-        fi
+        echo
     elif [[ "${release}" == "alpine" ]]; then
-        if [[ ${os_version} -lt 3.8 ]]; then
-            echo -e "${Error} 请使用 Alpine Linux 3.8 或更高版本！" && exit 1
-        fi
+        echo
     else
         echo -e "${Error} 抱歉，此脚本不支持您的操作系统。"
-        echo "${Info} 请确保您使用的是以下支持的操作系统之一："
-        echo "- Ubuntu 20.04+"
-        echo "- Debian 10+"
-        echo "- CentOS 7+"
-        echo "- Fedora 36+"
-        echo "- Arch Linux"
-        echo "- Parch Linux"
-        echo "- Manjaro"
-        echo "- Armbian"
-        echo "- AlmaLinux 9+"
-        echo "- Rocky Linux 9+"
-        echo "- Oracle Linux 8+"
-        echo "- Alpine Linux 3.8"
+        echo -e "${Info} 请确保您使用的是以下支持的操作系统之一："
+        echo -e "-${Red} Ubuntu${Nc} "
+        echo -e "-${Red} Debian ${Nc}"
+        echo -e "-${Red} CentOS ${Nc}"
+        echo -e "-${Red} Fedora ${Nc}"
+        echo -e "-${Red} Arch Linux ${Nc}"
+        echo -e "-${Red} Kali ${Nc}"
+        echo -e "-${Red} AlmaLinux ${Nc}"
+        echo -e "-${Red} Rocky Linux ${Nc}"
+        echo -e "-${Red} Oracle Linux ${Nc}"
+        echo -e "-${Red} Alpine Linux ${Nc}"
         exit 1
     fi
 }
 
-install_base(){
+check_pmc(){
     check_release
-    if [[ "$release" == "debian" || "$release" == "ubuntu" ]]; then
-        commands=("netstat")
-        apps=("net-tools")
-        install=()
-        for i in ${!commands[@]}; do
-            [ ! $(command -v ${commands[i]}) ] && install+=(${apps[i]})
-        done
-        [ "${#install[@]}" -gt 0 ] && apt update -y && apt install -y ${install[@]}
-    elif [[ "$release" == "centos" || "$release" == "fedora" ]]; then
-        commands=("netstat")
-        apps=("net-tools")
-        install=()
-        for i in ${!commands[@]}; do
-            [ ! $(command -v ${commands[i]}) ] && install+=(${apps[i]})
-        done
-        [ "${#install[@]}" -gt 0 ] && dnf update -y && dnf install -y ${install[@]}
-    else
-        echo -e "${Error} 很抱歉，你的系统不受支持！"
-        exit 1
+    if [[ "$release" == "debian" || "$release" == "ubuntu" || "$release" == "kali" ]]; then
+        updates="apt update -y"
+        installs="apt install -y"
+        app=( ["netstat"]="net-tools")
+    elif [[ "$release" == "almalinux" || "$release" == "fedora" || "$release" == "rocky" ]]; then
+        updates="dnf update -y"
+        installs="dnf install -y"
+        app=( ["netstat"]="net-tools")
+    elif [[ "$release" == "centos" || "$release" == "oracle" ]]; then
+        updates="yum update -y"
+        installs="yum install -y"
+        app=( ["netstat"]="net-tools")
+    elif [[ "$release" == "arch" ]]; then
+        updates="pacman -Syu --noconfirm"
+        installs="pacman -S --noconfirm"
+        app=( ["netstat"]="inetutils")
+    elif [[ "$release" == "alpine" ]]; then
+        updates="apk update -q"
+        installs="apk add -q"
+        app=( ["netstat"]="net-tools")
     fi
+}
+
+
+install_base(){
+    check_pmc
+    apps=("netstat")
+    install=()
+    for i in ${!commands[@]}; do
+        [ ! $(command -v ${apps[i]}) ] && install+=(${app[${apps[i]}]})
+    done
+    [ "${#install[@]}" -gt 0 ] && $updates && $installs ${install[@]}
 }
 
 set_port(){
@@ -139,10 +128,8 @@ set_passwd(){
 # 重启SSH服务
 restart_ssh(){
     check_release
-    if [[ "$release" == "debian" || "$release" == "ubuntu" || "$release" == "centos" || "$release" == "fedora" || "$release" == "almalinux" || "$release" == "rocky" || "$release" == "oracle" || "$release" == "manjaro" || "$release" == "parch" || "$release" == "arch" ]]; then
+    if [[ "$release" == "debian" || "$release" == "ubuntu" || "$release" == "centos" || "$release" == "fedora" || "$release" == "almalinux" || "$release" == "rocky" || "$release" == "oracle" || "$release" == "kali" || "$release" == "arch" ]]; then
         systemctl restart ssh* >/dev/null 2>&1
-    elif [[ "$release" == "armbian" ]]; then
-        service ssh* restart >/dev/null 2>&1
     elif [[ "$release" == "alpine" ]]; then
         rc-service ssh* restart >/dev/null 2>&1
     fi
